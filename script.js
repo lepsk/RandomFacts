7a6a872808cccdda5c8be63bcb82e44e5c97e576
Daniel <80263173+d0nka@users.noreply.github.com>
2023-09-23 21:58:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-let firstLoad = true;
-let timeout;
-
-function fetchFact() {
-    let factContainer = document.getElementById('fact-container');
-    let fact = document.getElementById('fact');
-    
-    factContainer.style.opacity = 0;
-    setTimeout(function() {
-        fact.textContent = firstLoad ? 'Loading Your Fact...' : 'Loading Your New Fact...';
-        factContainer.style.opacity = 1;
-    }, 500);
-
-    fetch('https://api.api-ninjas.com/v1/facts?limit=1', {
-        headers: {
-            'X-Api-Key': 'ziOppkPTJsYAZbIDohqy4zWDjNEbsLa2mzUslGth'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data && data[0] && data[0].fact) {
-            factContainer.style.opacity = 0;
-            setTimeout(function() {
-                fact.textContent = data[0].fact;
-                factContainer.style.opacity = 1;
-            }, 500);
-        } else {
-            fact.textContent = 'Fact not found in response.';
-        }
-        firstLoad = false;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        fact.textContent = 'An error occurred.';
-    });
+html {
+    background-color: #000;
 }
 
-fetchFact();
-document.getElementById('refresh').addEventListener('click', fetchFact);
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    font-family: 'Minimal', sans-serif;
+    background-color: #121212; /* 25% darker grey background */
+    margin: 0;
+    padding: 0;
+    position: relative;
+    transition: background-color 0.5s ease;
+}
 
-// Listen for mouse movement
-document.addEventListener('mousemove', function() {
-    clearTimeout(timeout);
-    document.body.style.backgroundColor = '#121212'; // dark grey
+#fact-container {
+    font-size: 24px;
+    text-align: center;
+    padding: 20px;
+    border-radius: 15px;
+    background-color: #111; /* even darker background */
+    color: #fff;
+    opacity: 0;
+    transition: opacity 0.5s ease-in-out;
+}
 
-    timeout = setTimeout(function() {
-        document.body.style.backgroundColor = '#000'; // black
-    }, 1000); // change back to black after 1 second of no mouse movement
-});
+#refresh {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+}
+
+#refresh:hover {
+    transform: scale(1.1);
+}
+
+@media only screen and (max-width: 600px) {
+    body {
+        font-size: 18px;
+    }
+
+    #fact-container {
+        font-size: 18px;
+        padding: 10px;
+    }
+
+    #refresh {
+        width: 40px;
+        height: 40px;
+    }
+}
